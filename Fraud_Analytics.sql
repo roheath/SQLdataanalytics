12b61517b823445bfd4fd63dab5bc4c5a0636380
Rohitm <rohitmetri24@gmail.com>
2024-09-03 19:53:54 +0530

    Delete Fraud_Analytics.sql

--- Fraud_Analytics.sql
+++
@@ -1,8 +0,0 @@
-use bank ;
-
-with rolling_fraud as ( SELECT nameorig,step, 
-SUM(isfraud) OVER (PARTITION BY nameOrig order by STEP ROWS BETWEEN 4 PRECEDING and CURRENT ROW ) as fraud_rolling
-FROM transactions)
-
-SELECT * FROM rolling_fraud
-WHERE fraud_rolling > 0 ;
